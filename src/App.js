44be88f726d79d144b760993cea84bad8e494b7f
SolBellomo <sbellomo@udesa.edu.ar>
2021-09-13 09:41:25 -0300

    probando app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
+import React, {Component} from 'react';
 import './App.css';
-import React from 'react';
 import Head from './components/Head/Head'
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Footer from './components/Footer/Footer';
 
-function App() {
-  return (
-    <>
-      <div className="App">
-        <Head />
-        <Header />
-        <Main />
+class App extends Component {
+  /*constructor (props){
+    super(props);
+    this.state = {
+      filterField: false, 
+    };
+  }
+  filterTrigger(){
+    if(this.state.filterField === false){
+      this.setState({filterField: true})
+    }
+    else {
+      this.setState({filterField:false})
+    }
+  }*/
+  render() {
+    return (
+      <div>
+        <div className="App">
+          <Head />
+          <Header /*filterField={this.state.filterField} filterTrigger={() => this.filterTrigger()}*//>
+          <Main /*filterField={this.state.filterField}*//>
+        </div>
+        <Footer /> 
       </div>
-      <Footer /> 
-    </>
-  );
+    );
+  }  
 }
 
 export default App;
